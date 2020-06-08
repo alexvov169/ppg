f8467313a6aecb6d7688c456e5478e1f70bac6cc
Oleksii Vovchok <a.vovchok.169@gmail.com>
2020-06-08 13:07:12 +0300

    cleanup and json test add

--- grammar/core.lisp
+++ grammar/core.lisp
@@ -1,10 +1,66 @@
 (uiop:define-package :ppg/grammar/core
   (:use :cl)
+  (:import-from :alexandria
+		#:alist-hash-table)
   (:export #:build-bnf-bindings))
 
 (in-package :ppg/grammar/core)
 
 
+(defstruct rule
+  name directives alternatives)
+
+(defclass terminal ()
+  ())
+
+(defclass lambda-terminal (terminal)
+  ((function :initarg :function
+	     :reader terminal-function)))
+
+(defclass named-terminal (terminal)
+  ((name :initarg :name
+	 :reader terminal-name)))
+
+(defstruct alt-part
+  limit terminal)
+
+(defparameter +terminal-directive+
+  (alist-hash-table '((:limit t))))
+
+(defun build-alt-part (alt-part)
+  (flet ((%terminal (alt-part)
+	   (cond ((and (consp alt-part))
+		  (case (first alt-part)
+		    (:predicate `(make-instance 'lambda-terminal :function ,(second alt-part)))))
+		 (t `(make-instance 'named-terminal :name ',alt-part)))))
+    (cond ((consp alt-part)
+	   (or (%terminal alt-part)
+	       (when (gethash (first alt-part) +terminal-directive+)
+		 (destructuring-bind (terminal-directive terminal &optional value) alt-part
+		   (case terminal-directive
+		     (:limit `(make-alt-part :limit ,value
+					     :terminal ,(%terminal terminal)))
+		     (otherwise (error "unhandled terminal directive ~A"
+				       terminal-directive))))))))))
+
+(defun build-alternative (alternative)
+  `(list ,@(mapcar #'build-alt-part alternative)))
 
 (defun build-bnf-bindings (bnf-bindings)
-  bnf-bindings)
+  `(,@ (mapcar (lambda (x)
+		 (loop :for i :in x
+		       :if (consp i)
+			 :collect i :into alternatives
+		       :else :if (keywordp i)
+			       :collect i :into directives
+		       :else :collect i :into rule-name
+		       :finally (return (if (rest rule-name)
+					    (error "extra symbols ~{~A~^ ~} are supplied in the rule"
+						   rule-name)
+					    `(,(first rule-name)
+					      (make-rule
+					       :name ',(first rule-name)
+					       :directives ',directives
+					       :alternatives (list ,@(mapcar #'build-alternative
+									     alternatives))))))))
+	       bnf-bindings)))
--- tests/generated.lisp
+++ tests/generated.lisp
@@ -1,10 +1,82 @@
 (uiop:define-package :ppg/tests/generated
   (:use :cl :rove)
-  (:use :ppg/generate/all))
+  (:use :ppg/grammar/all))
 
 (in-package :ppg/tests/generated)
 
 
 (deftest test-parse-digits
-    (testing ""
-	     t))
+  (testing "parallel parse validity"
+    (with-grammar ((term (char)
+			 (char (:limit term 10)))
+		   (:token char (:predicate #'digit-char-p)))
+      (let ((input (generate term)))
+	(ok (equalp (parse input :as term
+				 :worker-count 1)
+		    (parse input :as term
+				 :worker-count 4)))))))
+
+(deftest test-json
+  (testing "JSON paralel parse check"
+    (with-grammar ((json (value))
+
+		   (value (string)
+			  (number)
+			  (obj)
+			  (arr)
+			  ("true")
+			  ("false")
+			  ("null"))
+
+		   (obj ("{" pair pairs "}")
+			("{" "}"))
+
+		   (arr ("[" value values "]")
+			("[" "]"))
+
+		   (values ("," value values))
+
+		   (pair (string ":" value))
+
+		   (pairs (pair pairs))
+
+		   (:tokens tokens ((:none-or-many )))
+
+		   (ws (#\Space)
+		       (#\Tab)
+		       (#\Newline)
+		       (#\Return))
+
+		   (:token token
+			   (string)
+			   (number)
+			   ((:immediate)))
+
+		   (string (#\" (:none-or-many string-char) #\"))
+
+		   (esc ("\\b")
+			("\\f")
+			("\\n")
+			("\\r")
+			("\\t"))
+		   
+		   (string-char ((:range "\\u0000" "\\u001F"))
+				((:predicate #'identity)))
+
+		   (number (simple-num (:optional exponent)))
+
+		   (simple-num ((:optional "-") digits))
+
+		   (exponent ("e" simple-num)
+			     ("E" simple-num))
+
+		   (digits (digit digits)
+			   (digit))
+
+		   (digit ((:predicate #'digit-char-p))))
+
+      (let ((input (generate term)))
+	(ok (equalp (parse input :as term
+				 :worker-count 1)
+		    (parse input :as term
+				 :worker-count 4)))))))
